53f0f257e1e12ae6de070643dc6ad1449fc2faf2
ryf123 <ryf123@gmail.com>
2016-01-24 09:57:11 -0800

    percolation

---
+++ percolation/Percolation.java
@@ -0,0 +1,108 @@
+public class Percolation {
+	public int[][] points;
+	// create N-by-N grid, with all sites blocked
+	public int size;
+	public int[] roots;
+	public int[] weight;
+	public Percolation(int N){
+	    points = new int[N][N];
+	    size = N;
+	    roots = new int[N*N];
+	    for(int i=0;i<N*N;i++){
+	    	roots[i] = i;
+	    }
+	    weight = new int[N*N];
+	    java.util.Arrays.fill(weight,1);
+	}
+	// open site (row i, column j) if it is not open already
+	public void open(int i, int j) {
+	   if(i<=0 || i>size || j<=0 || j>size)
+		   throw new java.lang.IllegalArgumentException();		
+		i-=1;
+		j-=1;
+		int[][] positions = new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
+		if(points[i][j] == 0){
+		   points[i][j] =1;
+		   int[] p1 = new int[]{i,j};
+		   for(int count=0;count<positions.length;count++){
+			   int x = i+positions[count][0];
+			   int y = j+positions[count][1];
+			   if(x>=0 && x<size && y>=0 && y<size && isFull(x+1,y+1)){
+				   int[] p2 = new int[]{x,y};
+				   connect(p1,p2);
+			   }
+		   }
+		   for(int count=0;count<positions.length;count++){
+			   int x = i+positions[count][0];
+			   int y = j+positions[count][1];
+			   if(x>=0 && x<size && y>=0 && y<size && isOpen(x+1,y+1)){
+				   int[] p2 = new int[]{x,y};
+				   connect(p1,p2);
+			   }
+		   }
+		}
+	}         
+	
+	// is site (row i, column j) full?
+	public boolean isFull(int i, int j){
+	   if(i<=0 || i>size || j<=0 || j>size)
+		   throw new java.lang.IllegalArgumentException();
+	   i-=1;
+	   j-=1;
+	   if(root(index(i,j)) < size && points[i][j]==1)return true;
+	   return false;
+	}     
+	// is site (row i, column j) open?
+	public boolean isOpen(int i, int j){
+	   if(i<=0 || i>size || j<=0 || j>size)
+		   throw new java.lang.IllegalArgumentException();
+	   if(isFull(i,j))return false;
+	   i-=1;
+	   j-=1;
+	   return points[i][j] ==1;
+	}     
+	// does the system percolate?
+	public boolean percolates(){
+	   for(int i=0;i<size;i++){
+		   if(points[size-1][i] != 0)
+			   if(isFull(size,i+1))
+				   return true;
+	   }
+	   return false;
+	}
+	private int index(int i,int j){
+		return i*size+j;
+	}
+	private int root(int id){
+		while(roots[id] != id){
+			roots[id] = roots[roots[id]];
+			id=roots[id];
+		}
+		return id;
+	}
+	private void connect(int[] point1,int[] point2){
+		int index1 = index(point1[0],point1[1]);
+		int index2 = index(point2[0],point2[1]);
+		int root1 = root(index1);
+		int root2 = root(index2);
+		if(root1 != root2){
+			if(root1 < size){
+				roots[root2] = root1;
+			}
+			else if(root2 < size){
+				roots[root1] = root2;
+			}
+			else if(weight[root1] >= weight[root2]){
+				roots[root2] = root1;
+				weight[root1] += weight[root2];
+			}
+			else{
+				roots[root1] = root2;
+				weight[root2] += weight[root1];
+			}
+		}
+	}
+	public static void main(String[] args){
+	   
+	}  
+}
---
+++ percolation/PercolationStats.java
@@ -0,0 +1,69 @@
+public class PercolationStats {
+   public double[] thresholds;
+   int size;
+   int total;
+   // perform T independent experiments on an N-by-N grid
+   public PercolationStats(int N, int T){
+	   size = N;
+	   total = T;
+	   thresholds = new double[T];
+	   for(int i=0;i<T;i++){
+		   Percolation pc = new Percolation(N);
+		   while(pc.percolates() == false){
+			   int x = edu.princeton.cs.algs4.StdRandom.uniform(N);
+			   int y = edu.princeton.cs.algs4.StdRandom.uniform(N);
+			   pc.open(x+1, y+1);
+		   }
+		   thresholds[i] = ratio(pc,N);
+	   }
+	   java.util.Arrays.sort(thresholds);
+//	   System.out.printf("mean%-20s=%.12f\n"," ",mean());
+//	   System.out.printf("stddev%-20s=%.12f\n"," ",stddev());
+//	   double low  = confidenceLo();
+//	   double high = confidenceHi();
+//	   System.out.printf("95 confidence interval: %.12f,%.12f\n",low,high);
+   }
+   // sample mean of percolation threshold
+   public double mean(){
+	   return edu.princeton.cs.algs4.StdStats.mean(thresholds);
+   }
+   // sample standard deviation of percolation threshold
+   public double stddev(){
+	   return edu.princeton.cs.algs4.StdStats.stddev(thresholds);
+   }                    
+   // low  endpoint of 95% confidence interval
+   public double confidenceLo(){
+	   Double low = total*0.05;
+	   double[] temp = java.util.Arrays.copyOfRange(thresholds,0,thresholds.length-low.intValue());
+	   return edu.princeton.cs.algs4.StdStats.mean(temp);
+   }
+   // high endpoint of 95% confidence interval
+   public double confidenceHi(){
+	   Double low = total*0.05;
+	   double[] temp = java.util.Arrays.copyOfRange(thresholds,low.intValue(),thresholds.length-1);
+	   return edu.princeton.cs.algs4.StdStats.mean(temp);	   
+   }              
+   private double ratio(Percolation pc,int N){
+	   int count = 0;
+	   for(int i=0;i<N;i++){
+		   for(int j=0;j<N;j++){
+			   
+			   if (pc.isOpen(i+1, j+1) || pc.isFull(i+1, j+1)){
+				   count++;
+			   }
+		   }
+	   }
+	   double ret = 1.0*count/(N*N);
+	   return ret;
+   }
+   public static void main(String[] args){
+	   if (args.length == 2){
+		   int N = Integer.parseInt(args[0]);
+		   int T = Integer.parseInt(args[1]);
+		   edu.princeton.cs.algs4.Stopwatch sw = new edu.princeton.cs.algs4.Stopwatch();
+		   PercolationStats ps = new PercolationStats(N,T);
+		   System.out.printf("time eclapsed: %.2f\n", sw.elapsedTime());
+	   } 
+
+   }    // test client (described below)
+}
